ddfce941025a09ddc401f81afe960188ece653f6
fejoa <ryanvanherel@gmail.com>
2016-11-07 00:03:35 +1300

    Werewolf wolves can give victims lycanthropy via biting, and deal double damage to vampires.

--- Main/Source/human.cpp
+++ Main/Source/human.cpp
@@ -3836,6 +3836,31 @@ festring werewolfwolf::GetKillName() const
   return humanoid::GetKillName();
 }
 
+truth werewolfwolf::SpecialBiteEffect(character* Victim, v2 HitPos, int BodyPartIndex, int Direction, truth BlockedByArmour, truth Critical, int DoneDamage)
+{
+  if(!BlockedByArmour && Victim->IsWarmBlooded() && (!(RAND() % 2) || Critical) && !Victim->AllowSpoil())
+  {
+    // Werewolf wolf gives lycanthropy
+    if(Victim->IsHumanoid() && !Victim->StateIsActivated(VAMPIRISM) && !Victim->StateIsActivated(LYCANTHROPY))
+      Victim->BeginTemporaryState(LYCANTHROPY, 6000 + RAND_N(2000));
+
+    // Werewolves do double damage against vampires and this is a drain attack
+    if(Victim->StateIsActivated(VAMPIRISM) && (DoneDamage >= 1))
+    {
+      if(IsPlayer())
+        ADD_MESSAGE("You drain some life force from %s!", Victim->CHAR_DESCRIPTION(DEFINITE));
+      else if(Victim->IsPlayer() || Victim->CanBeSeenByPlayer() || CanBeSeenByPlayer())
+        ADD_MESSAGE("%s drains some life force from %s!", CHAR_DESCRIPTION(DEFINITE), Victim->CHAR_DESCRIPTION(DEFINITE));
+
+      return Victim->ReceiveBodyPartDamage(this, DoneDamage, DRAIN, BodyPartIndex, Direction);
+    }
+    else
+      return false;
+  }
+  else
+    return false;
+}
+
 int humanoid::GetRandomApplyBodyPart() const
 {
   if(RightArmIsUsable())
