d0a3217cdba3c07a8147bdff4a1f321dfed04bd0
fejoa <ryanvanherel@gmail.com>
2016-11-27 18:02:59 +1300

    Add vampire bats
     - These are dark blue-grey with red eyes, and occasionally do a drain attack.
     - Only appear in Xinroch tomb and Attnamese catacombs.
     - Old bats are now dark brown with yellow eyes :)
     - Note the change to Char.pcx.

--- Main/Source/nonhuman.cpp
+++ Main/Source/nonhuman.cpp
@@ -922,6 +922,29 @@ truth spider::SpecialBiteEffect(character* Char, v2, int, int, truth BlockedByAr
     return false;
 }
 
+truth vampirebat::SpecialBiteEffect(character* Victim, v2 HitPos, int BodyPartIndex, int Direction, truth BlockedByArmour, truth Critical, int DoneDamage)
+{
+  if(!BlockedByArmour && Victim->IsWarmBlooded() && (!(RAND() % 3) || Critical) && !Victim->AllowSpoil())
+  {
+    if(IsPlayer())
+      ADD_MESSAGE("You drain some precious lifeblood from %s!", Victim->CHAR_DESCRIPTION(DEFINITE));
+    else if(Victim->IsPlayer() || Victim->CanBeSeenByPlayer() || CanBeSeenByPlayer())
+      ADD_MESSAGE("%s drains some precious lifeblood from %s!", CHAR_DESCRIPTION(DEFINITE), Victim->CHAR_DESCRIPTION(DEFINITE));
+
+    if(Victim->IsHumanoid() && !Victim->StateIsActivated(LYCANTHROPY))
+      Victim->BeginTemporaryState(VAMPIRISM, 500 + RAND_N(250));
+
+      // HP recieved is about half the damage done; against werewolves this is full
+      int DrainDamage = (DoneDamage >> 1) + 1;
+      if(Victim->StateIsActivated(LYCANTHROPY))
+        DrainDamage = DoneDamage + 1;
+
+    return Victim->ReceiveBodyPartDamage(this, DrainDamage, DRAIN, BodyPartIndex, Direction);
+  }
+  else
+    return false;
+}
+
 truth chameleon::SpecialEnemySightedReaction(character*)
 {
   if(HP != MaxHP || !(RAND() % 3))
