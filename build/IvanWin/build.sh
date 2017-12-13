61174f8ee0a0df37f4a22afe69f5e57ddb694656
fejoa <ryanvanherel@gmail.com>
2017-12-13 14:31:36 +1300

    Configure sdl2 for mxe

--- build/IvanWin/build.sh
+++ build/IvanWin/build.sh
@@ -8,6 +8,9 @@ if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
     MXE_TARGET=i686-w64-mingw32.static
 fi
 
+pkg-config --cflags sdl2
+pkg-config --libs sdl2
+
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
 cmake --build build-dir --config Release
 
