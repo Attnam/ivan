4880761248cc030333fddfe440b90a1788bb518f
fejoa <ryanvanherel@gmail.com>
2017-12-13 16:36:45 +1300

    Turn shared libs off in cmake

--- build/IvanWin/build.sh
+++ build/IvanWin/build.sh
@@ -11,7 +11,7 @@ fi
 pkg-config --cflags sdl2
 pkg-config --libs --static sdl2
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
 cmake --build build-dir --config Release
 
 set +xue
