b5315b6142a1a4e08dd12f043b3c68288aa61de9
fejoa <6005108+fejoa@users.noreply.github.com>
2017-12-14 13:21:23 +1300

    cmake with --debug-output

--- build/IvanWin/build.sh
+++ build/IvanWin/build.sh
@@ -12,6 +12,6 @@ fi
 /usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --libs --static sdl2
 
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
-cmake --build build-dir --config Release
+cmake --build build-dir --debug-output --config Release
 
 set +xue
