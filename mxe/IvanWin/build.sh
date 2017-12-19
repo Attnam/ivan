93c84b828e2174a34444426598e15f7463daf5db
fejoa <ryanvanherel@gmail.com>
2017-12-19 16:50:28 +1300

    Remove space between |

--- mxe/IvanWin/build.sh
+++ mxe/IvanWin/build.sh
@@ -8,7 +8,8 @@ if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
     MXE_TARGET=i686-w64-mingw32.static
     SDL2_PKGCONFIG_CFLAGS=$(/usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --cflags sdl2)
     SDL2_PKGCONFIG_LFLAGS=$(/usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --libs --static sdl2)
-    SDL2_PKGCONFIG_LFLAGS2=$(echo "$SDL2_PKGCONFIG_LFLAGS" | sed 's/^[ \t]*//;s/[ \t]*$//')
+    SDL2_PKGCONFIG_LFLAGS2=$(echo "$SDL2_PKGCONFIG_LFLAGS"| sed 's/^[ \t]*//;s/[ \t]*$//')
+    #SDL2_PKGCONFIG_LFLAGS2=$(echo "$SDL2_PKGCONFIG_LFLAGS"| awk '{$1=$1};1')
 fi
 
 #${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
