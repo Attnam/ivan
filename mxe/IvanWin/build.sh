83663ad3e62ca4287c843f15e587d9d36a3d9222
fejoa <ryanvanherel@gmail.com>
2017-12-19 13:24:42 +1300

    Dodge SDL2.dll when compiling statically
    Also set SDL2 static linker flags to use the output of i686-w64-mingw32.static-pkg-config --libs --static sdl2

--- mxe/IvanWin/build.sh
+++ mxe/IvanWin/build.sh
@@ -6,13 +6,12 @@ MXE_DIR=/usr/lib/mxe
 
 if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
     MXE_TARGET=i686-w64-mingw32.static
+    SDL2_PKGCONFIG_CFLAGS = /usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --cflags sdl2
+    SDL2_PKGCONFIG_LFLAGS = /usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --libs --static sdl2
 fi
 
-/usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --cflags sdl2
-/usr/lib/mxe/usr/bin/i686-w64-mingw32.static-pkg-config --libs --static sdl2
-
 #${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DCMAKE_CXX_FLAGS="-Werror -Wno-format-security -Wno-undefined-var-template -Wno-unknown-warning-option $CXXFLAGS" -DSDL2_LDFLAGS=SDL2_PKGCONFIG_LFLAGS
 #cd build
 #cmake --build build-dir --config Release
 #cd build-dir
