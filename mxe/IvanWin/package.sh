0378680d4215f5780c546edb612f98f6abd2a158
fejoa <ryanvanherel@gmail.com>
2017-12-20 15:50:57 +1300

    Remove IVAN_FILE_VERSION

--- mxe/IvanWin/package.sh
+++ mxe/IvanWin/package.sh
@@ -4,8 +4,8 @@ set -xue
 
 if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
   cd ${TRAVIS_BUILD_DIR}/build/IvanWin/
-  mv ivan ${IVAN_PLATFORM}${IVAN_FILE_VERSION}
-  zip -9 -r ${IVAN_PLATFORM}${IVAN_FILE_VERSION}.zip *
+  mv ivan ${IVAN_PLATFORM}
+  zip -9 -r ${IVAN_PLATFORM}.zip *
 fi
 
 set +xue
