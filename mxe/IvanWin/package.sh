9c3e3e0d9b9e3922285399361070fe663b98d31f
fejoa <6005108+fejoa@users.noreply.github.com>
2017-12-20 21:43:21 +1300

    Add -j to zip

--- mxe/IvanWin/package.sh
+++ mxe/IvanWin/package.sh
@@ -5,7 +5,7 @@ set -xue
 if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
   cd ${TRAVIS_BUILD_DIR}/build/IvanWin/
   sudo mv ivan ${IVAN_PLATFORM}
-  sudo zip -9 -r ${IVAN_PLATFORM}.zip *
+  sudo zip -9 -r -j ${IVAN_PLATFORM}.zip *
 fi
 
 set +xue
