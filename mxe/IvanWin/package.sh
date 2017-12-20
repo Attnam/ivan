7fff641e666269561296a17041521105b2b89a71
fejoa <ryanvanherel@gmail.com>
2017-12-20 16:16:25 +1300

    Make cmake_policy(SET CMP0004 OLD) optional

--- mxe/IvanWin/package.sh
+++ mxe/IvanWin/package.sh
@@ -4,7 +4,7 @@ set -xue
 
 if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
   cd ${TRAVIS_BUILD_DIR}/build/IvanWin/
-  mv ivan ${IVAN_PLATFORM}
+  sudo mv ivan ${IVAN_PLATFORM}
   zip -9 -r ${IVAN_PLATFORM}.zip *
 fi
 
