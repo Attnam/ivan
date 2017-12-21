87a3579eafcf3ddcb5a6b71d1970d1ffa9c6240d
fejoa <ryanvanherel@gmail.com>
2017-12-22 12:41:10 +1300

    Export IVAN_FILE_VERSION in version.sh

--- mxe/IvanWin/version.sh
+++ mxe/IvanWin/version.sh
@@ -5,9 +5,9 @@ set -xue
 CURRENT_GIT_TAG=$(git name-rev --name-only --tags HEAD)
 
 if [ "$CURRENT_GIT_TAG" = "undefined" ]; then
-  IVAN_FILE_VERSION="-unversioned-$(git rev-parse --short HEAD)-$(date +%Y%m%d%H%M%S)"
+  export IVAN_FILE_VERSION="-unversioned-$(git rev-parse --short HEAD)-$(date +%Y%m%d%H%M%S)"
 else
-  IVAN_FILE_VERSION="${CURRENT_GIT_TAG#?}"
+  export IVAN_FILE_VERSION="${CURRENT_GIT_TAG#?}"
 fi
 
 set +xue
