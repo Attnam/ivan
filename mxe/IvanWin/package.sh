c70c3ec5b3315916aa8012215c2a975ede3ad842
fejoa <ryanvanherel@gmail.com>
2017-12-22 16:28:44 +1300

    Remove spurious comments

--- mxe/IvanWin/package.sh
+++ mxe/IvanWin/package.sh
@@ -4,7 +4,6 @@ set -xue
 
 if [ "$IVAN_PLATFORM" = "IvanWin" ]; then
   cd ${TRAVIS_BUILD_DIR}/build/IvanWin/ivan
-#  sudo mv ivan ${IVAN_PLATFORM}${IVAN_FILE_VERSION}
   sudo zip -9 -r ${IVAN_PLATFORM}${IVAN_FILE_VERSION}.zip *
 fi
 
