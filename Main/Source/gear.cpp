6b701eda1a497a9fe573b479358a7369e9efc28a
red-kangaroo <david.vlach2@seznam.cz>
2017-12-24 12:35:13 +0100

    Display AV for shields

--- Main/Source/gear.cpp
+++ Main/Source/gear.cpp
@@ -645,10 +645,8 @@ void shield::AddInventoryEntry(ccharacter* Viewer, festring& Entry,
 
   if(ShowSpecialInfo)
   {
-    Entry << " [" << GetWeight() << "g, " << GetBaseBlockValueDescription();
-
-    if(!IsBroken())
-      Entry << ", " << GetStrengthValueDescription();
+    Entry << " [" << GetWeight() << "g, AV " << GetStrengthValue();
+    Entry << ", " << GetBaseBlockValueDescription();
 
     int CWeaponSkillLevel = Viewer->GetCWeaponSkillLevel(this);
     int SWeaponSkillLevel = Viewer->GetSWeaponSkillLevel(this);
