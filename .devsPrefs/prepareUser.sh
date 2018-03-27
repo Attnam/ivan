4056c757d01730fc4c6f19b002cc9f67108aca37
AquariusPower <AquariusPower@users.noreply.github.com>
2018-03-27 00:39:31 -0300

    added script to automate symlinks creations on linux: prepareUser.sh

---
+++ .devsPrefs/prepareUser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ ! -L Current ]];then echo "must be run at .devsPrefs folder!";exit 1;fi
+
+strUser="`readlink Current`"
+declare -p strUser
+
+cd ..
+pwd
+
+ls -l .gitignore .cproject .project
+
+ln -vsf .devsPrefs/Current/.cproject .cproject
+ln -vsf .devsPrefs/Current/.project .project
+ln -vsf ".devsPrefs/Current/.gitignore.$strUser" .gitignore
+
+ls -l .gitignore .cproject .project
