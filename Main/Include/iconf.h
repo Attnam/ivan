5f4bcb777d47969e8ba73773635ccfb6e29b292d
AquariusPower <AquariusPower@users.noreply.github.com>
2018-06-01 00:26:42 -0300

    (ready) GoOn can now ignore items (#372)
    * goOn now can ignore items
    
    * goOn ignore items: fix missing iMaxValueless
    
    * GoOn ignoring items: fix for doors/chests

--- Main/Include/iconf.h
+++ Main/Include/iconf.h
@@ -29,6 +29,7 @@ class ivanconfig
   static long GetStartingWindowWidth() { return iStartingWindowWidth; }
   static long GetStartingWindowHeight() { return iStartingWindowHeight; }
   static long GetFrameSkip() { return FrameSkip.Value; }
+  static long GetGoOnStopMode() { return GoOnStopMode.Value; }
   static long GetHoldPosMaxDist() { return HoldPosMaxDist.Value; }
   static truth IsSavegameSafely(){ return SavegameSafely.Value; }
   static truth IsAllowImportOldSavegame(){ return AllowImportOldSavegame.Value; }
@@ -85,6 +86,7 @@ class ivanconfig
   static void AutoSaveIntervalDisplayer(const numberoption*, festring&);
   static void XBRZSquaresAroundPlayerDisplayer(const numberoption* O, festring& Entry);
   static void ShowMapDisplayer(const cycleoption* O, festring& Entry);
+  static void GoOnStopModeDisplayer(const cycleoption* O, festring& Entry);
   static void ShowItemsAtPlayerSquareDisplayer(const cycleoption* O, festring& Entry);
   static void RotateTimesPerSquareDisplayer(const cycleoption* O, festring& Entry);
   static void AltSilhouettePreventColorGlitchDisplayer(const cycleoption* O, festring& Entry);
@@ -192,6 +194,7 @@ class ivanconfig
 
   static numberoption XBRZSquaresAroundPlayer;
   static cycleoption DirectionKeyMap;
+  static cycleoption GoOnStopMode;
   static cycleoption SilhouetteScale;
   static cycleoption AltSilhouette;
   static cycleoption AltListItemPos;
