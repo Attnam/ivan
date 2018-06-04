e8f6d86dfa8f8020d1bdfda43f8984b45f84f6fb
AquariusPower <AquariusPower@users.noreply.github.com>
2018-06-03 22:30:20 -0300

    prepended "(WIZ)" if enabled on savegame entries

--- FeLib/Source/feio.cpp
+++ FeLib/Source/feio.cpp
@@ -1141,7 +1141,7 @@ festring iosystem::ContinueMenu(col16 TopicColor, col16 ListColor,
       if(bSaveGameSortModeByDtTm)
         id<<fsVer<<rfi.time<<" ";
 
-      id<<sPrettyNameWork.c_str()<<" ";
+      id<<sPrettyNameWork.c_str()<<(rfi.WizardMode?" (WIZ)":"")<<" ";
 
       if(!bSaveGameSortModeByDtTm)
         id<<fsVer<<" "; //after to not compromise the alphanumeric default sorting in case user want's to use it
