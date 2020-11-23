cda5822586f71a40cb1f18c618b0534c918c6e56
fejoa <6005108+fejoa@users.noreply.github.com>
2020-11-24 09:55:45 +1300

    Another integer size re-balance
    Test to see if subsequent push triggers actions

--- audio/midiplayback.cpp
+++ audio/midiplayback.cpp
@@ -233,7 +233,7 @@ void MPB_TogglePlayback(MIDI_HEADER_CHUNK_t* MIDIHdr)
 
 void MPB_ResetMIDITracks(MIDI_HEADER_CHUNK_t* MIDIHdr)
 {
-   uint8_t i;
+   uint16_t i;
    uint8_t* position = 0;
    uint8_t* buf;
    uint8_t* ptr;
