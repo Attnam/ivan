0a6f68abdc4bbda316eb02bb160b6a02491e4bf7
J.W <iology@users.noreply.github.com>
2022-03-21 21:01:59 +0000

    Release build for legacy macos (10.9+) on the latest MacOS (11.6.2) (#643)
    * Fix typo
    
    * Try to build for legacy macos

--- ci/osx/requirements.sh
+++ ci/osx/requirements.sh
@@ -8,6 +8,9 @@ fi
 
 CACHE_DIR="${SDL2DIR}"
 
+# directory of this script
+SCRIPT_DIR=$(dirname -- "$0")
+
 brew_install() {
   for dep in "$@"; do
     if brew list "${dep}" &>/dev/null; then
@@ -21,39 +24,60 @@ brew_install() {
 }
 
 install_sdl2() {
+  local dirs=(
+    'SDL2.framework'
+    'SDL2_mixer.framework'
+  )
   local urls=(
-    'https://libsdl.org/release/SDL2-2.0.9.dmg'
+    'https://libsdl.org/release/SDL2-2.0.20.dmg'
     'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.dmg'
   )
 
   mkdir -p "${CACHE_DIR}"
-  if [[ "${CACHE_DIR}" = "$(find "${CACHE_DIR}" -maxdepth 0 -type d -empty)" ]]
-  then
-    local tmpdir=$(mktemp -d)
+
+  local i
+  for i in "${!urls[@]}"; do
+    local dir="${dirs[$i]}" url="${urls[$i]}"
+
+    if [[ -d "${CACHE_DIR}/${dir}" ]]; then
+      continue
+    fi
+
+    local tmpdir="$(mktemp -d)"
+    trap 'hdiutil detach sdl >/dev/null 2>&1 || true; rm -Rf "${tmpdir}"' RETURN
     pushd "${tmpdir}"
 
-    local url
-    for url in "${urls[@]}"; do
-      curl -L --fail --retry 5 -o sdl.dmg "${url}"
-      hdiutil attach -readonly -mount required -mountpoint sdl sdl.dmg
-      cp -R sdl/* "${CACHE_DIR}"
-      hdiutil detach sdl
-      rm sdl.dmg
-    done
+    curl -L --fail --retry 5 -o sdl.dmg "${url}"
+    hdiutil attach -readonly -mount required -mountpoint sdl sdl.dmg
+    cp -R sdl/"${dir}" "${CACHE_DIR}"
+    hdiutil detach sdl
+    rm sdl.dmg
 
     popd
     rm -Rf "${tmpdir}"
-  fi
+  done
 
   echo "Cached Frameworks:"
   ls -hl "${CACHE_DIR}"
 }
 
 if [[ -n "${IVAN_PLATFORM}" || "${BUILD_MAC_APP}" = ON ]]; then
-  if [[ -n "${TRAVIS_TAG}" ]]; then
-    brew update  # for deployment
+  ## pcre and libpng are quiet stable, the CI servers can do better here
+  #if [[ -n "${TRAVIS_TAG}" || "${TODO_BREW_UPDATE}" = ON ]]; then
+  #  brew update  # for deployment
+  #fi
+  brew_install pkg-config cmake
+  if [[ -n "${MACOSX_DEPLOYMENT_TARGET}" ]]; then
+    # dyld: lazy symbol binding failed: Symbol not found: ____chkstk_darwin
+    for formula in "${SCRIPT_DIR}"/{pcre,libpng}.rb; do
+      # brew will sanitize environment variables, therefore...
+      sed -i.orig "s/X_MACOSX_DEPLOYMENT_TARGET/${MACOSX_DEPLOYMENT_TARGET}/g" "${formula}"
+      brew reinstall --force --build-from-source --formula "${formula}"
+      mv "${formula}"{.orig,}
+    done
+  else
+    brew_install pcre libpng
   fi
-  brew_install pkg-config cmake pcre libpng
   install_sdl2
 else
   brew_install pkg-config cmake pcre libpng sdl2 sdl2_mixer
