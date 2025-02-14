d0c170907631e38c0e3758d9681e0ddb8cc085f9
ryfactor <6005108+ryfactor@users.noreply.github.com>
2025-02-14 18:28:37 +1300

    Update message.cpp

--- Main/Source/message.cpp
+++ Main/Source/message.cpp
@@ -53,7 +53,7 @@ void msgsystem::AddMessage(cchar* Format, ...)
 
   va_list AP;
   va_start(AP, Format);
-  vsprintf(Message, Format, AP);
+  vsnprintf(Message, sizeof(Message), Format, AP);
   va_end(AP);
 
   festring Buffer(Message);
